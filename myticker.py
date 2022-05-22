ce656f8ee0ab34d622990241a43c4f0e575d8ac8
DrDavidL <92898146+DrDavidL@users.noreply.github.com>
2022-05-21 22:12:11 -0500

    Ask for input - stock symbol

--- myticker.py
+++ myticker.py
@@ -3,13 +3,13 @@ import yfinance as yf
 import streamlit as st
 
 st.write("""
-# Simple Stock Price App
+# Demo Stock Price App - many more data options possible! 
 Shown are the stock closing price and volume of Google!
 """)
 
 # https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
 #define the ticker symbol
-tickerSymbol = 'GOOGL'
+tickerSymbol = st.text_input('Enter your ticker symbol!')
 #get data on this ticker
 tickerData = yf.Ticker(tickerSymbol)
 #get the historical prices for this ticker

555aaedd9907de4f813b728ffdf3e46c505f4308
DrDavidL <92898146+DrDavidL@users.noreply.github.com>
2022-05-25 18:24:40 -0500

    input validation added to BMI and Prime

--- pages/.ipynb_checkpoints/bmiapp-checkpoint.py
+++ pages/.ipynb_checkpoints/bmiapp-checkpoint.py
@@ -1,21 +1,57 @@
 import streamlit as st
 
+#def isfloat(list):
+#    if len(list) == 0:
+#        return
+#   else:
+#        for n in list:
+#            try:
+#                isok = float(n)
+#                except ValueError:
+#                    st.write("Please enter numbers only; not other characters.")
+    
+
 def bmiapp():
     st.title("BMI Calculator")
     st.write('*Use pounds and inches!!!*')
-    heightstr = st.text_input('Enter your height in inches:')
-    weightstr = st.text_input('Enter your weight in pounds:')
+    heightstr = st.text_input('Step 1. Please enter your height in inches:')
+    weightstr = st.text_input('Step 2. Please enter your weight in pounds:')
     
-    if len(weightstr) != 0 and len(heightstr) !=0:
 
-        height = float(heightstr)
-        weight = float(weightstr)
+    
+    
+   # testing = isfloat(inputlist)
+   
+    if len(heightstr) != 0 and len(weightstr) != 0:
+        try:
+            heightf = float(heightstr)
+        except ValueError:
+            st.write("Please enter a numerical value for height.")
+            return
+        
+        try:
+            weightf = float(weightstr)
+        except ValueError:
+            st.write("Please enter a numerical value for weight.")
+            return
 
-        weightkg = weight * 0.453592
-        heightcm = height * 2.54
 
-        bmi = weightkg / (heightcm/100)**2
+        if float(heightstr) > 96 or float(heightstr) < 20 or float(weightstr) > 600 or float(weightstr) < 10:
+            st.write("Your values are out of range. Please check your inches and pounds!")
+        else:
 
-        st.write("# Your BMI is:", round(bmi,1))
+
+
+            height = float(heightstr)
+            weight = float(weightstr)
+
+
+
+            weightkg = weight * 0.453592
+            heightcm = height * 2.54
+
+            bmi = weightkg / (heightcm/100)**2
+
+            st.write("# Your BMI is:", round(bmi,1))
 
         # print(round(bmi,1))
--- pages/.ipynb_checkpoints/primes-checkpoint.py
+++ pages/.ipynb_checkpoints/primes-checkpoint.py
@@ -5,32 +5,40 @@ def primes():
     st.write('*Limiting processer for input < 10000!!!*')
     
     submission = st.text_input('Enter a number')
+
+        
     if len(submission) !=0:
-        n = int(submission)
-        if n >= 10000:
-            st.write("Your number was too large!")
-
+        try:
+            number = int(submission)
+        except ValueError:
+            st.write("Please enter a valid input: a positive integer < 10000.")
+    
         else:
-            isPrime = {}
-            primes=[]
-            
+            n = int(submission)
+            if n >= 10000:
+                st.write("Your number was too large!")
 
-            # Establish range of interest.
-            for i in range(2, n+1):
-                isPrime[i] = True
+            else:
+                isPrime = {}
+                primes=[]
 
-            # Mark the values false that are not prime.    
-            for j in range(2, int(n**0.5+1)):
-                k = 0
-                notprime = j**2
-                if isPrime[j] == True:
-                    while notprime <= n:
-                        isPrime[notprime] = False
-                        notprime = j**2 + k*j                
-                        k += 1
 
-            # Finalize your list of just prime values.           
-            for n in isPrime:
-                if isPrime[n] == True:
-                    primes.append(n)
-            st.success(primes)
+                # Establish range of interest.
+                for i in range(2, n+1):
+                    isPrime[i] = True
+
+                # Mark the values false that are not prime.    
+                for j in range(2, int(n**0.5+1)):
+                    k = 0
+                    notprime = j**2
+                    if isPrime[j] == True:
+                        while notprime <= n:
+                            isPrime[notprime] = False
+                            notprime = j**2 + k*j                
+                            k += 1
+
+                # Finalize your list of just prime values.           
+                for n in isPrime:
+                    if isPrime[n] == True:
+                        primes.append(n)
+                st.success(primes)
